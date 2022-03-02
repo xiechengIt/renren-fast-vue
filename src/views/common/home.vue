385f550f01158c5324c543e9d59dcc64699c7818
Aynik <1820862808@qq.com>
2022-03-03 02:52:40 +0800

    daily commit

--- src/views/common/home.vue
+++ src/views/common/home.vue
@@ -1,38 +1,145 @@
 <template>
   <div>
     <div style="font-size: 40px" align="center">
-      <span>公司简介</span><br /><span style="font-size: 1.1rem; color: #999"
-        >INTRODUCTION</span
-      >
+      <span>计划编辑</span><br />
     </div>
-    <div style="font-size: 20px" align="center">
-      <div class="about_top">
-        <p>
-          &nbsp; &nbsp;
-          宁波德昌电机制造有限公司，成立于2002年1月，是一家符合产业发展方向的规模以上企业，厂房占地面积135亩，建筑面积150000平方米，致力于电机和家用吸尘器的研发生产，产品主要面向欧美市场，通过UL、CE、CCC等认证，符合欧盟ROHS、REACH指令要求，是宁波市级高新技术企业，全国吸尘器电机产品质量公认十佳品牌，浙江省著名商标，宁波市重点出口企业，宁波市智能制造协会理事单位。被列入“宁波市高成长企业培育计划”。
-        </p>
-        <p>
-          &nbsp; &nbsp;
-          公司已成功研制开发YDC系列电机30余款，年生产电机能力达1000万台。2012年与国际知名品牌建立战略合作，生产研发吸尘器整机，拥有全自动注塑机300台以及其他各类先进的配套设备。生产的立式水洗吸尘器集吸尘、清洗和吸干一步完成的功能，产品达到高质量水平，填补了此行业的空白。
-          2018年销售额达12亿元，是余姚市出口创汇第二名，宁波市前五十强，被评为“全国吸尘器十大出口企业”。2019年销售额保持12.7亿高位，宁波进出口排行83名。
-        </p>
-        <p>
-          &nbsp; &nbsp;
-          唯有不断探索，才能不断成长。公司从未停止探索企业发展的脚步。2017年，成立子公司宁波德昌科技有限公司，打造数字车间、智能工厂，进一步开拓国内市场，致力于汽车EPS无刷电机的研发生产；2018年与国际知名品牌合作，研发生产美容美发产品；2019年成立子公司宁波德昌电器有限公司，进军家庭健康护理行业，实现产品多元化发展。
-        </p>
-        <p style="text-align: center">
-          <img
-            style="width: 50%; display: block; margin: 0 auto"
-            src="~@/assets/img/1588731410133493.jpg"
-          />
-        </p>
-      </div>
-    </div>
+    <el-form
+      :model="ruleForm"
+      :rules="rules"
+      ref="ruleForm"
+      label-width="100px"
+      class="demo-ruleForm"
+    >
+      <el-form-item label="活动名称" prop="name">
+        <el-input v-model="ruleForm.name"></el-input>
+      </el-form-item>
+      <el-form-item label="活动区域" prop="region">
+        <el-select v-model="ruleForm.region" placeholder="请选择活动区域">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="活动时间" required>
+        <el-col :span="11">
+          <el-form-item prop="date1">
+            <el-date-picker
+              type="date"
+              placeholder="选择日期"
+              v-model="ruleForm.date1"
+              style="width: 100%"
+            ></el-date-picker>
+          </el-form-item>
+        </el-col>
+        <el-col class="line" :span="2">-</el-col>
+        <el-col :span="11">
+          <el-form-item prop="date2">
+            <el-time-picker
+              placeholder="选择时间"
+              v-model="ruleForm.date2"
+              style="width: 100%"
+            ></el-time-picker>
+          </el-form-item>
+        </el-col>
+      </el-form-item>
+      <el-form-item label="即时配送" prop="delivery">
+        <el-switch v-model="ruleForm.delivery"></el-switch>
+      </el-form-item>
+      <el-form-item label="活动性质" prop="type">
+        <el-checkbox-group v-model="ruleForm.type">
+          <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
+          <el-checkbox label="地推活动" name="type"></el-checkbox>
+          <el-checkbox label="线下主题活动" name="type"></el-checkbox>
+          <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
+        </el-checkbox-group>
+      </el-form-item>
+      <el-form-item label="特殊资源" prop="resource">
+        <el-radio-group v-model="ruleForm.resource">
+          <el-radio label="线上品牌商赞助"></el-radio>
+          <el-radio label="线下场地免费"></el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="活动形式" prop="desc">
+        <el-input type="textarea" v-model="ruleForm.desc"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')"
+          >立即创建</el-button
+        >
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      ruleForm: {
+        name: "",
+        region: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: "",
+      },
+      rules: {
+        /*name: [
+          { required: true, message: "请输入活动名称", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" },
+        ] ,
+        region: [
+          { required: true, message: "请选择活动区域", trigger: "change" },
+        ],
+        date1: [
+          {
+            type: "date",
+            required: true,
+            message: "请选择日期",
+            trigger: "change",
+          },
+        ],
+        date2: [
+          {
+            type: "date",
+            required: true,
+            message: "请选择时间",
+            trigger: "change",
+          },
+        ],
+        type: [
+          {
+            type: "array",
+            required: true,
+            message: "请至少选择一个活动性质",
+            trigger: "change",
+          },
+        ],
+        resource: [
+          { required: true, message: "请选择活动资源", trigger: "change" },
+        ],
+        desc: [{ required: true, message: "请填写活动形式", trigger: "blur" }],*/
+      }, 
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          ax
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+  },
+};
 </script>
 
 <style>
